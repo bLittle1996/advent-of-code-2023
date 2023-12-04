4307507cdd88f4fbd85919b4ebc378d7c27f9c4b
Ben Littleton <b.little1996@gmail.com>
2023-12-04 11:30:19 -0500

    feat: main function executes point sum calculation against puzzle input

--- day-4-scratchcards/main.go
+++ day-4-scratchcards/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/bLittle1996/advent-of-code-2023/scratchcards/data"
+	"github.com/bLittle1996/advent-of-code-2023/scratchcards/scratchcard"
+)
+
 func main() {
-	panic("at the disco")
+	scratchcards := scratchcard.NewPile(data.Data)
+	pointSum := 0
+
+	for _, sc := range scratchcards {
+		pointSum += sc.Points()
+	}
+
+	fmt.Println(fmt.Sprintf("From all %d cards, you have a total of %d points!", len(scratchcards), pointSum))
 }
