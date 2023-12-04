412960f5ae9595fcd293dc30d16ce9bd7b05f012
Ben Littleton <b.little1996@gmail.com>
2023-12-04 13:03:02 -0500

    chore: typo

--- day-4-scratchcards/main.go
+++ day-4-scratchcards/main.go
@@ -11,6 +11,6 @@ func main() {
 	scratchcards := scratchcard.NewPile(data.Data)
 
 	fmt.Println(fmt.Sprintf("From all %d scratchcards, you have a total of %d points!", len(scratchcards), scratchcards.Points()))
-	fmt.Println("But points aren't everything, in fact that's now how these scratchcards works at all!")
+	fmt.Println("But points aren't everything, in fact that's now how these scratchcards work at all!")
 	fmt.Println(fmt.Sprintf("You actually just win more scratchcards, %d in your case, and those scratchcards don't even win you anything useful! Ha!", len(scratchcards.Process())))
 }
