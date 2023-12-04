47a05c37dc088ff81138196e7d743dfb77334db1
Ben Littleton <b.little1996@gmail.com>
2023-12-04 11:21:41 -0500

    feat: added parsing of inputs, added tests to assert single line parsing works as expected

--- day-4-scratchcards/scratchcard/scratchcard.go
+++ day-4-scratchcards/scratchcard/scratchcard.go
@@ -1,6 +1,11 @@
 package scratchcard
 
-import "math"
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+)
 
 type Scratchcard struct {
 	Id int
@@ -37,13 +42,132 @@ func (s Scratchcard) Points() int {
 // Card Id: WinningNumbers | CardNumbers
 //
 // i.e. `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53`
-func New(cardInput string) Scratchcard {
+func New(cardInput string) (Scratchcard, error) {
 	s := Scratchcard{}
 
-	return s
+	// []{"Card 1"," 41 ... 17 | 83 ... 53"}
+	parts := strings.Split(strings.Trim(cardInput, "\n"), ":")
+
+	if len(parts) != 2 {
+		return s, fmt.Errorf("invalid input: %s", cardInput)
+	}
+
+	id, err := parseCardId(parts[0])
+
+	if err != nil {
+		return s, err
+	}
+
+	s.Id = id
+	s.WinningNumbers, s.CardNumbers = parseNumbers(parts[1])
+
+	return s, nil
 }
 
 // NewPile functions as New but processes multiple lines of input at once.
+//
+// If an error is encountered when parsing a specific line, it is ignored.
+// This means it is possible to have an empty slice returned, if every line is invalid.
 func NewPile(cardsInput string) []Scratchcard {
-	return []Scratchcard{}
+	cardPile := []Scratchcard{}
+	lines := strings.Split(strings.Trim(cardsInput, "\n"), "\n")
+
+	for _, line := range lines {
+		if s, err := New(line); err == nil { // if no error
+			cardPile = append(cardPile, s)
+		}
+	}
+
+	return cardPile
+}
+
+// parseCardId returns the number present in a string of the following format: Card %d
+func parseCardId(cardIdSegment string) (int, error) {
+	idParts := strings.Split(cardIdSegment, " ")
+
+	if len(idParts) != 2 || idParts[0] != "Card" {
+		return 0, fmt.Errorf("cannot parse card id: %s", cardIdSegment)
+	}
+
+	id, err := strconv.Atoi(idParts[1])
+
+	if err != nil {
+		return 0, fmt.Errorf("cannot parse card id: %w", err)
+	}
+
+	return id, nil
+}
+
+// parseNumbers returns []int slices from a string of the following format: %d %d ... %d | %d ... %d
+//
+// If no numbers could be parsed, empty slices may be returned
+func parseNumbers(numbersSegment string) ([]int, []int) {
+	parts := strings.Split(strings.TrimSpace(numbersSegment), "|")
+
+	if len(parts) != 2 {
+		return []int{}, []int{}
+	}
+
+	// turn the string of numbers into a []string{} with extra spaces removed from each entry
+	winningNumbersStr := strings.TrimSpace(parts[0])
+	cardNumbersStr := strings.TrimSpace(parts[1])
+	winningNumbers := mapSlice(strings.Split(winningNumbersStr, " "), strings.TrimSpace)
+	cardNumbers := mapSlice(strings.Split(cardNumbersStr, " "), strings.TrimSpace)
+	// lets remove non-digit elements
+	winningNumbers = filterSlice(winningNumbers, onlyDigits)
+	cardNumbers = filterSlice(cardNumbers, onlyDigits)
+
+	// and finally convert them into numbers proper
+	return mapSlice(winningNumbers, atoi), mapSlice(cardNumbers, atoi)
+}
+
+// filterSlice returns a new slice containing only elements that match the predicate
+func filterSlice[T any](slice []T, predicate func(T) bool) []T {
+	filtered := []T{}
+
+	for _, v := range slice {
+		if predicate(v) {
+			filtered = append(filtered, v)
+		}
+	}
+
+	return filtered
+}
+
+// mapSlice returns a new slice with the result of a mapping function applied to each element
+func mapSlice[T any, R any](slice []T, mapper func(T) R) []R {
+	mapped := []R{}
+
+	for _, v := range slice {
+		mapped = append(mapped, mapper(v))
+	}
+
+	return mapped
+}
+
+// isDigit determines if a character is a digit (0-9)
+func isDigit(char rune) bool {
+	return char >= '0' && char <= '9'
+}
+
+// onlyDigits determines if a string consists of only digit characters (0-9)
+func onlyDigits(str string) bool {
+	if len(str) == 0 {
+		return false
+	}
+
+	for _, char := range str {
+		if !isDigit(char) {
+			return false
+		}
+	}
+
+	return true
+}
+
+// atoi is as strconv.Atoi, but ignores the error
+func atoi(str string) int {
+	n, _ := strconv.Atoi(str)
+
+	return n
 }
--- day-4-scratchcards/scratchcard/scratchcard_test.go
+++ day-4-scratchcards/scratchcard/scratchcard_test.go
@@ -1,6 +1,7 @@
 package scratchcard_test
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/bLittle1996/advent-of-code-2023/scratchcards/scratchcard"
@@ -11,10 +12,53 @@ func TestThatItSolvesTheExampleScratchcard(t *testing.T) {
 	scratchcard := scratchcard.Scratchcard{
 		Id:             1,
 		WinningNumbers: []int{41, 48, 83, 86, 17},
-		CardNumbers:    []int{83, 83, 6, 31, 17, 9, 48, 53},
+		CardNumbers:    []int{83, 86, 6, 31, 17, 9, 48, 53},
 	}
 
 	if scratchcard.Points() != expectedPoints {
 		t.Errorf("Expected %d, received %d", expectedPoints, scratchcard.Points())
+	}
+}
+
+func Test_New_ReturnsScratchcardForValidInput(t *testing.T) {
+	type testCase struct {
+		input                  string
+		expectedId             int
+		expectedWinningNumbers []int
+		expectedCardNumbers    []int
+		expectedPoints         int
+	}
+
+	cases := []testCase{
+		{"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53", 1, []int{41, 48, 83, 86, 17}, []int{83, 86, 6, 31, 17, 9, 48, 53}, 8},
+		{"Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19", 2, []int{13, 32, 20, 16, 61}, []int{61, 30, 68, 82, 17, 32, 24, 19}, 2},
+		{"Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1", 3, []int{1, 21, 53, 59, 44}, []int{69, 82, 63, 72, 16, 21, 14, 1}, 2},
+		{"Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83", 4, []int{41, 92, 73, 84, 69}, []int{59, 84, 76, 51, 58, 5, 54, 83}, 1},
+		{"Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36", 5, []int{87, 83, 26, 28, 32}, []int{88, 30, 70, 12, 93, 22, 82, 36}, 0},
+		{"Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11", 6, []int{31, 18, 13, 56, 72}, []int{74, 77, 10, 23, 35, 67, 36, 11}, 0},
+	}
+
+	for i, tc := range cases {
+		s, err := scratchcard.New(tc.input)
+
+		if err != nil {
+			t.Errorf("Test Case %d New: %s", i, err.Error())
+		}
+
+		if s.Id != tc.expectedId {
+			t.Errorf("Test Case %d: expected id %d, got %d", i, tc.expectedId, s.Id)
+		}
+
+		if !reflect.DeepEqual(s.CardNumbers, tc.expectedCardNumbers) {
+			t.Errorf("Test Case %d: expected CardNumbers %v, got %v", i, tc.expectedCardNumbers, s.CardNumbers)
+		}
+
+		if !reflect.DeepEqual(s.WinningNumbers, tc.expectedWinningNumbers) {
+			t.Errorf("Test Case %d: expected CardNumbers %v, got %v", i, tc.expectedWinningNumbers, s.WinningNumbers)
+		}
+
+		if s.Points() != tc.expectedPoints {
+			t.Errorf("Test Case %d: expected points %d, got %d", i, tc.expectedPoints, s.Points())
+		}
 	}
 }
